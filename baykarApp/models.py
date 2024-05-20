c4948b63d506663a5951e4462c2583efcb059178
Ömer Faruk Yanık <omerfarukyanik212313@gmail.com>
2024-05-20 06:15:04 +0300

    base model for drone added

--- baykarApp/models.py
+++ baykarApp/models.py
@@ -1,1 +1,8 @@
 from django.db import models
+
+
+class Product(models.Model):
+    brand = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image_url = models.URLField()
